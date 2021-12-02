5049337c4232cfb4b0cc647f3dc6a55d52d745d4
juanmah98 <halo-hola@hotmail.com>
2021-12-02 19:27:47 -0300

    cambio2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://jsonplaceholder.typicode.com',
-  url: 'http://demo8931173.mockable.io/'
+  url: 'http://demo8931173.mockable.io'
 };
